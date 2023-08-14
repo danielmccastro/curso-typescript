629ce056eeb877e47cad59f8808c31781f885be6
Daniel Castro <danielmccastro@gmail.com>
2023-08-14 17:49:47 -0300

    Tipo any

---
+++ src/A0002-tipo-any/A0002-tipo-any.ts
@@ -0,0 +1,9 @@
+// utilize any apenas em ultimo caso
+
+function showMesage(msg: any) {
+  return msg;
+}
+
+console.log(showMesage([1, 2, 3]));
+console.log(showMesage('Ola'));
+console.log(showMesage(1));
