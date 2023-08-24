cd87b7525320ba22e90f2959a3768151eb59a28e
Daniel Castro <danielmccastro@gmail.com>
2023-08-24 16:10:47 -0300

    A0032 & A0033

---
+++ src/A0033-implementando-tipos/A0033-implementando-tipos.ts
@@ -0,0 +1,46 @@
+/* type TipoPessoa = {
+  nome: string;
+  sobrenome: string;
+  nomeCompleto: () => string;
+};
+
+export class Pessoa implements TipoPessoa {
+  constructor(
+    public nome: string,
+    public sobrenome: string,
+  ) {}
+
+  nomeCompleto(): string {
+    return this.nome + ' ' + this.sobrenome;
+  }
+}
+
+const pessoa = new Pessoa('Luiz', 'Miranda');
+console.log(pessoa.nomeCompleto()); */
+
+type TipoNome = {
+  nome: string;
+  sobrenome: string;
+};
+
+type TipoSobrenome = {
+  sobrenome: string;
+};
+
+type TipoNomeCompleto = {
+  nomeCompleto: () => string;
+};
+
+export class Pessoa implements TipoNome, TipoSobrenome, TipoNomeCompleto {
+  constructor(
+    public nome: string,
+    public sobrenome: string,
+  ) {}
+
+  nomeCompleto(): string {
+    return this.nome + ' ' + this.sobrenome;
+  }
+}
+
+const pessoa = new Pessoa('Luiz', 'Miranda');
+console.log(pessoa.nomeCompleto());
