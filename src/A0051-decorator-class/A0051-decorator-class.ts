6eb8d1de5bd3011fb6a3acd92f6e5190ee9d9df2
Daniel Castro <danielmccastro@gmail.com>
2023-08-28 18:20:03 -0300

    A0051 to A0058

---
+++ src/A0051-decorator-class/A0051-decorator-class.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+@decorator
+export class Animal {
+  constructor(
+    public nome: string,
+    public cor: string,
+  ) {}
+}
+
+function decorator<T extends new (...args: any[]) => any>(target: T): T {
+  return class extends target {
+    cor: string;
+    nome: string;
+    constructor(...args: any[]) {
+      super(...args);
+      this.nome = this.inverte(args[0]);
+      this.cor = this.inverte(args[1]);
+    }
+    inverte(valor: string): string {
+      return valor.split('').reverse().join('');
+    }
+  };
+}
+
+const animal = new Animal('Luiz', 'roxo');
+console.log(animal);
